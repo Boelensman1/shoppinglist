60cfae9b3c92be52d5d967d5745d38a6b630715a
Boelensman1 <me@wigger.email>
2025-10-05 13:54:52 +0200

    WIP of push notifications
    Signed-off-by: Boelensman1 <me@wigger.email>

--- next/src/app/page.tsx
+++ next/src/app/page.tsx
@@ -1,10 +1,16 @@
+'use client'
+
 import { StoreProvider } from '@/store/useStore'
 import App from './app'
+import { InstallPrompt } from '@/components/InstallPrompt'
+import { PushNotificationManager } from '@/components/PushNotificationManager'
 
 export default function Home() {
   return (
     <StoreProvider>
       <App />
+      <PushNotificationManager />
+      <InstallPrompt />
     </StoreProvider>
   )
 }
