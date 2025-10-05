04c68529eba8578397dee6a99476d6bfcc43faa4
Boelensman1 <me@wigger.email>
2025-10-05 14:04:29 +0200

    Remove basepath
    Signed-off-by: Boelensman1 <me@wigger.email>

--- next/next.config.ts
+++ next/next.config.ts
@@ -5,7 +5,6 @@ const isProd = process.env.NODE_ENV === 'production'
 const nextConfig: NextConfig = {
   distDir: 'build',
   output: isProd ? 'standalone' : undefined,
-  basePath: process.env.NODE_ENV === 'production' ? '/shoppinglist' : '',
 }
 
 export default nextConfig
