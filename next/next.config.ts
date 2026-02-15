1aba56bc05ff07aab7a1e8d6bbdb794d85e59ba7
Boelensman1 <me@wigger.email>
2026-02-15 08:46:21 +0100

    Improve sw config
    Signed-off-by: Boelensman1 <me@wigger.email>

--- next/next.config.ts
+++ next/next.config.ts
@@ -1,10 +1,16 @@
 import type { NextConfig } from 'next'
+import { spawnSync } from 'node:child_process'
 import withSerwistInit from '@serwist/next'
 
 const isProd = process.env.NODE_ENV === 'production'
 const swDisabled = process.env.NEXT_PUBLIC_DISABLE_SW === '1'
 
+const revision = swDisabled
+  ? ''
+  : spawnSync('git', ['rev-parse', 'HEAD'], { encoding: 'utf-8' }).stdout
+
 const withSerwist = withSerwistInit({
+  additionalPrecacheEntries: [{ url: '/~offline', revision }],
   cacheOnNavigation: true,
   swSrc: 'src/app/sw.ts',
   swDest: 'public/sw.js',
